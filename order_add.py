5f2da4511a59a8a80c838a8f2676af8355233fa2
DonDigidon9 <mercver@yandex.ru>
2024-11-18 19:53:38 +0300

    unvisible listbox

--- order_add.py
+++ order_add.py
@@ -44,6 +44,11 @@ def orders_add(app,
     title_window = "Добавить заказ"
     if order_register_date_fill != "":
         title_window = "Изменить заказ"
+
+    customer_listbox_visible = False
+    timber_listbox_visible = False
+    status_listbox_visible = False
+
     layout = [
         # TODO: выровнить все элементы и подобрать подходящий цвет для мест с датами
         [sg.Text(title_window, justification='center', font=font_title, size=(monitor.width, 2),
@@ -56,10 +61,13 @@ def orders_add(app,
          sg.InputText(default_text=order_accomplishment_date_fill, size=(15, 1), font=font_button, key='-DATA_COMPLETION-', disabled_readonly_background_color='green', disabled=True),
          sg.Button("Дата", font=font_button, key='-DATA_COM-')],
         [sg.Push()],
-        [sg.Text("Заказчик:", font=font_button, size=(25, 2)), sg.Listbox(mas_customer, font=font_button, size=(100, 4))],
-        [sg.Text("Вид лесопродукции:", font=font_button, size=(25, 2)), sg.Listbox(mas_timber_product, font=font_button, size=(100, 4))],
+        [sg.Text("Заказчик:", font=font_button, size=(25, 2)), sg.Listbox(mas_customer, visible=False, font=font_button, size=(100, 4), key='-CUSTOMER_LISTBOX-'),
+         sg.Button("Показать варианты", key="-TOGGLE_CUSTOMER-", font=font_button)],
+        [sg.Text("Вид лесопродукции:", font=font_button, size=(25, 2)), sg.Listbox(mas_timber_product, visible=False, font=font_button, size=(100, 4), key='-TIMBER_LISTBOX-'),
+         sg.Button("Показать варианты", key="-TOGGLE_TIMBER-", font=font_button)],
         [sg.Text("Количество лесопродукции:", font=font_button, size=(25, 2)), sg.InputText(product_amount_fill, size=(15, 1), font=font_button, key="-INPUT-")],
-        [sg.Text("Статус:", font=font_button, size=(25, 2)), sg.Listbox(mas_status, font=font_button, size=(100, 4))],
+        [sg.Text("Статус:", font=font_button, size=(25, 2)), sg.Listbox(mas_status, visible=False, font=font_button, size=(100, 4), key='-STATUS_LISTBOX-'),
+         sg.Button("Показать варианты", key="-TOGGLE_STATUS-", font=font_button)],
         [sg.Text("Комментарий:", font=font_button, size=(25, 2)), sg.Multiline(default_text=comment_fill, font=font_button, size=(100, 4), key='-COMMENT-')],
         [sg.Push()],
         [sg.Button("Сохранить", font=font_button)],
@@ -106,6 +114,18 @@ def orders_add(app,
                 file.write(json_data)
             window.close()
             return app
+        elif event == '-TOGGLE_CUSTOMER-':
+            customer_listbox_visible = not customer_listbox_visible
+            window["-CUSTOMER_LISTBOX-"].update(visible=customer_listbox_visible)
+            window["-TOGGLE_CUSTOMER-"].update("Скрыть варианты" if customer_listbox_visible else "Показать варианты")
+        elif event == '-TOGGLE_TIMBER-':
+            timber_listbox_visible = not timber_listbox_visible
+            window["-TIMBER_LISTBOX-"].update(visible=timber_listbox_visible)
+            window["-TOGGLE_TIMBER-"].update("Скрыть варианты" if timber_listbox_visible else "Показать варианты")
+        elif event == '-TOGGLE_STATUS-':
+            status_listbox_visible = not status_listbox_visible
+            window["-STATUS_LISTBOX-"].update(visible=status_listbox_visible)
+            window["-TOGGLE_STATUS-"].update("Скрыть варианты" if status_listbox_visible else "Показать варианты")
         elif event == '-DATA_REG-':
             selected_data_reg = sg.popup_get_date()
             if selected_data_reg:
